69ad500c32f9249f1c67a0a6058b46aad65446f8
KevinLozano07 <kevin.lozano@tecnologicoliceocrisitano.edu.ec>
2024-07-26 14:45:39 -0500

    Actividad en clase

---
+++ Actividades/Actividad_en_clase/Actividad_25/7/2024.py
@@ -0,0 +1,14 @@
+#Realizar listas y presentarlas 
+
+Lista_de_compras = ["Tocino", "Huevos", "Pan", "Jugo de naranja"]
+
+print("")
+for Compras in Lista_de_compras:
+    print(Compras)
+print("")
+
+Materias = ["Matematicas", "Biologia", "ECA", "Lenguaje", "Computación", "Contabilidad"]
+
+print("")
+print(Materias[1:5])
+print("")
